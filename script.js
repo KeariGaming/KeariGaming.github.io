2ffb153c347c992c5d24f7cfc29564cc297d673b
KeariGaming <keari0010@gmail.com>
2026-03-14 18:03:30 -0500

    Update download link handling in asset cards

--- script.js
+++ script.js
@@ -9,15 +9,27 @@ fetch("assets.json")
         const card = document.createElement("div");
         card.className = "asset-card";
 
+        let downloadURL = "";
+        let downloadAttr = "";
+
+        if(asset.file){
+            downloadURL = "assets/" + asset.file;
+            downloadAttr = "download";
+        }
+        else if(asset.link){
+            downloadURL = asset.link;
+        }
+
         card.innerHTML = `
             <h3>${asset.name}</h3>
             <p>${asset.description}</p>
-            <a class="download" href="assets/${asset.file}" download>
+            <a class="download" href="${downloadURL}" ${downloadAttr}>
             Download
             </a>
         `;
 
         container.appendChild(card);
+
     });
 
 });
