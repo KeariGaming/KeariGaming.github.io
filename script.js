d9e1e92537c29f67b5ae97c5f6bce1a32fe3a2c2
KeariGaming <keari0010@gmail.com>
2026-03-14 18:17:13 -0500

    Refactor asset download URL logic in script.js

--- script.js
+++ script.js
@@ -3,46 +3,34 @@ fetch("assets.json")
 .then(data => {
 
     const container = document.getElementById("assetList");
-    container.innerHTML = "";
 
     data.assets.forEach(asset => {
 
         const card = document.createElement("div");
         card.className = "asset-card";
 
-        let downloadURL = "";
+        let downloadURL = asset.file ? "assets/" + asset.file : asset.link;
+
         let buttonClass = "download";
         let buttonText = "Download";
-        let downloadAttr = "";
 
-        if (asset.file) {
-            downloadURL = "assets/" + asset.file;
-            downloadAttr = "download";
-        }
-
-        if (asset.link) {
-            downloadURL = asset.link;
-        }
-
-        if (asset.popuplink) {
+        if (asset.popuplink === true) {
             buttonClass = "download-popup";
             buttonText = "Download (Pop Up Link)";
         }
 
-        const button = `
-        <a class="${buttonClass}" href="${downloadURL}" ${downloadAttr}>
-        ${buttonText}
-        </a>
-        `;
-
         card.innerHTML = `
             <h3>${asset.name}</h3>
             <p>${asset.description}</p>
-            ${button}
+            <a class="${buttonClass}" href="${downloadURL}">
+            ${buttonText}
+            </a>
         `;
 
         container.appendChild(card);
 
     });
+
+});
 
 });
