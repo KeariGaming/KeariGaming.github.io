b383d94172c5afb307d8e26d0d35b2381d7a7042
KeariGaming <keari0010@gmail.com>
2026-03-15 13:20:30 -0500

    Update script3d.js

--- script3d.js
+++ script3d.js
@@ -8,6 +8,22 @@ fetch("3d-assets.json")
         const card = document.createElement("div");
         card.className = "asset-card";
 
+        // Determine if it's a popup link or normal download
+        let buttonClass = "download";
+        let buttonText = "Download";
+        let buttonHref = `models/${asset.file}`;
+        let isDownload = true;
+
+        if (asset.popuplink) {
+            buttonClass = "download-popup";
+            buttonText = "Download (Pop Up Link)";
+        }
+
+        if (asset.link) {
+            buttonHref = asset.link;
+            isDownload = false; // It’s a normal link now
+        }
+
         card.innerHTML = `
         <model-viewer class="model-viewer"
             src="models/${asset.file}"
@@ -19,7 +35,7 @@ fetch("3d-assets.json")
         <h3>${asset.name}</h3>
         <p>${asset.description}</p>
         <div class="asset-size">${asset.size}</div>
-        <a class="download" href="models/${asset.file}" download>Download</a>
+        <a class="${buttonClass}" href="${buttonHref}" ${isDownload ? "download" : 'target="_blank"'}>${buttonText}</a>
         `;
 
         container.appendChild(card);
